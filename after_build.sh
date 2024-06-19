153d07467bc0aea46e00daa13a830160d1f65279
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-06-19 12:05:44 +0200

    Update script to handle cosmetics

--- after_build.sh
+++ after_build.sh
@@ -1,11 +1,16 @@
 rm -rf BuildOutput &&
 mkdir BuildOutput &&
-cp -f "$CURRENT_PROJECT"/bin/Debug/netstandard2.1/TestAccount666."$CURRENT_PROJECT".dll BuildOutput/"$CURRENT_PROJECT".dll &&
+mkdir BuildOutput/BepInEx/ &&
+mkdir BuildOutput/BepInEx/plugins/ &&
+mkdir BuildOutput/BepInEx/plugins/TestAccount666."$CURRENT_PROJECT"/ &&
+mkdir BuildOutput/BepInEx/plugins/TestAccount666."$CURRENT_PROJECT"/MoreCompanyCosmetics &&
+cp -f "$CURRENT_PROJECT"/bin/Debug/netstandard2.1/TestAccount666."$CURRENT_PROJECT".dll BuildOutput/BepInEx/plugins/TestAccount666."$CURRENT_PROJECT"/"$CURRENT_PROJECT".dll &&
 cp -f "$CURRENT_PROJECT"/README.md BuildOutput/ &&
 cp -f "$CURRENT_PROJECT"/CHANGELOG.md BuildOutput/ &&
 cp -f "$CURRENT_PROJECT"/icon.png BuildOutput/ &&
 cp -f LICENSE BuildOutput/ &&
-cp -f voidleak BuildOutput/ &&
+cp -f voidleak BuildOutput/BepInEx/plugins/TestAccount666."$CURRENT_PROJECT"/ &&
+cp -f VoidLeak.cosmetics BuildOutput/BepInEx/plugins/TestAccount666."$CURRENT_PROJECT"/MoreCompanyCosmetics/ &&
 ./generate_manifest.sh &&
 ./generate_zipfile.sh &&
 dolphin "./BuildOutput"
